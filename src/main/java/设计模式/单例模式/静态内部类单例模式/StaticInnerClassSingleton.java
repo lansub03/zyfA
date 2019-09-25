c3655b279adcc4463d895874c65d9bfcc0d77b43
git_xuanxuan <779247257@qq.com>
2019-09-25 17:01:47 +0800

    增加静态内部类单例模式

---
+++ src/main/java/设计模式/单例模式/静态内部类单例模式/StaticInnerClassSingleton.java
@@ -0,0 +1,14 @@
+package 设计模式.单例模式.静态内部类单例模式;
+
+public class StaticInnerClassSingleton {
+    private StaticInnerClassSingleton(){}
+
+    //单例持有者
+    private static class InnerClaa{
+        private final static StaticInnerClassSingleton scsl = new StaticInnerClassSingleton();
+    }
+
+    public static StaticInnerClassSingleton getStaticInnerClassSingleton(){
+        return InnerClaa.scsl;
+    }
+}
