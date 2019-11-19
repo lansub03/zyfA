380375fec0527a114a669af10ae3fd53710372fd
git_xuanxuan <779247257@qq.com>
2019-11-20 01:10:23 +0800

    增加myabtis接口调用sql 的用法

--- src/main/java/框架/Mybatis/src/main/java/client.java
+++ src/main/java/框架/Mybatis/src/main/java/client.java
@@ -1,3 +1,4 @@
+import dao.UserMapper;
 import domain.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -27,7 +28,23 @@ public class client {
         System.out.println("info:"+user);
      }
 
-     //使用接口的方式调用myabtis
+     //使用接口调用mapper映射文件中的sql
+     @Test
+     public void testImplMybatis() throws IOException {
+         //1、加载myabtis配置文件 读取myabtis配置文件
+         InputStream resourceAsStream = Resources.getResourceAsStream("SqlMapConfig.xml");
 
+         //2、使用sqlSessionFactoryBuild来创建一个sqlSessionFactory
+         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+         //3、获取到sql session  进行调取api
+         SqlSession sqlSession = sqlSessionFactory.openSession();
+
+         //4、根据反射获取接口的对象
+         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+         User user = userMapper.selectByid2(1);
+         System.out.println("info:"+user);
+
+     }
 
 }
---
+++ src/main/java/框架/Mybatis/src/main/java/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package dao;
+
+import domain.User;
+
+public interface UserMapper {
+    User selectByid2(Integer id);
+}
